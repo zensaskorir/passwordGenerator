6c64e504f8478a72b18d5014536442eae03ad031
Zensas Korir <zensaskorir@gmail.com>
2025-08-26 15:09:35 +0300

    Commit my Password Generator

---
+++ script.js
@@ -0,0 +1,28 @@
+const passwordBox = document.getElementById("password");
+const long = 12;
+
+//Characters for Password
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "`~!@#$%^&*()_+<>?:}{\][';/.,'";
+
+const allChars = upperCase+lowerCase+number+symbols;
+
+function createPassword(){
+    let password="";
+    /* password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+ */
+    while (long>password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value=password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
